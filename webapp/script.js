7e6fcd704c73cd7a03a909d4094271131fd35b83
Michalis Iakovides <33570332+michalis111@users.noreply.github.com>
2023-04-04 21:23:03 +0100

    Add files via upload

---
+++ webapp/script.js
@@ -0,0 +1,95 @@
+let scene, camera, rendered, cube;
+
+function parentWidth(elem) {
+  return elem.parentElement.clientWidth;
+}
+
+function parentHeight(elem) {
+  return elem.parentElement.clientHeight;
+}
+
+function init3D(){
+  scene = new THREE.Scene();
+  scene.background = new THREE.Color(0xffffff);
+
+  camera = new THREE.PerspectiveCamera(75, parentWidth(document.getElementById("3Dcube")) / parentHeight(document.getElementById("3Dcube")), 0.1, 1000);
+
+  renderer = new THREE.WebGLRenderer({ antialias: true });
+  renderer.setSize(parentWidth(document.getElementById("3Dcube")), parentHeight(document.getElementById("3Dcube")));
+
+  document.getElementById('3Dcube').appendChild(renderer.domElement);
+
+  // Create a geometry
+  const geometry = new THREE.BoxGeometry(1, 1, 1);
+  // Materials of each face
+  var cubeMaterials = [
+    new THREE.MeshBasicMaterial({color:0x03045e}),
+    new THREE.MeshBasicMaterial({color:0x023e8a}),
+    new THREE.MeshBasicMaterial({color:0x0077b6}),
+    new THREE.MeshBasicMaterial({color:0x03045e}),
+    new THREE.MeshBasicMaterial({color:0x023e8a}),
+    new THREE.MeshBasicMaterial({color:0x0077b6}),
+  ];
+
+  const material = new THREE.MeshFaceMaterial(cubeMaterials);
+
+  cube = new THREE.Mesh(geometry, material);
+  scene.add(cube);
+  camera.position.z = 5;
+  renderer.render(scene, camera);
+}
+
+// Resize the 3D object when the browser window changes size
+function onWindowResize(){
+  camera.aspect = parentWidth(document.getElementById("3Dcube")) / parentHeight(document.getElementById("3Dcube"));
+  //camera.aspect = window.innerWidth /  window.innerHeight;
+  camera.updateProjectionMatrix();
+  //renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setSize(parentWidth(document.getElementById("3Dcube")), parentHeight(document.getElementById("3Dcube")));
+
+}
+
+window.addEventListener('resize', onWindowResize, false);
+
+// Create the 3D representation
+init3D();
+
+// Create events for the sensor readings
+if (!!window.EventSource) {
+  var source = new EventSource('/events');
+
+  source.addEventListener('open', function(e) {
+    console.log("Events Connected");
+  }, false);
+
+  source.addEventListener('error', function(e) {
+    if (e.target.readyState != EventSource.OPEN) {
+      console.log("Events Disconnected");
+    }
+  }, false);
+
+ 
+
+  source.addEventListener('Readings', function(e) {
+    console.log("Readings", e.data);
+    var obj = JSON.parse(e.data);
+    document.getElementById("steps").innerHTML = obj.steps;
+    document.getElementById("gyroX").innerHTML = obj.gyroX;
+    document.getElementById("gyroY").innerHTML = obj.gyroY;
+    document.getElementById("gyroZ").innerHTML = obj.gyroZ;
+
+  // Change cube rotation after receiving the readings
+    cube.position.x = obj.steps;
+    cube.rotation.x = obj.gyroX;
+    cube.rotation.y = obj.gyroY;
+    cube.rotation.z = obj.gyroZ;
+    renderer.render(scene, camera);
+  }, false);
+}
+
+function resetPosition(element){
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", "/"+element.id, true);
+  console.log(element.id);
+  xhr.send();
+}
